4e2eacf139e1388f0e410abd835de36496a3e1ec
dengweixiong <cxycxy1111@gmail.com>
2020-04-08 16:50:30 +0800

    new code

---
+++ src/com/alfred/healthylife/Service/TagService.java
@@ -0,0 +1,33 @@
+package com.alfred.healthylife.Service;
+
+import com.alfred.healthylife.DAO.TagDAO;
+
+public class TagService extends Service {
+
+    private TagDAO tagDAO;
+
+    public TagService() {
+        tagDAO = new TagDAO();
+    }
+
+    public void create(String title) {
+
+    }
+
+    public void query(int page_no, int num_lmt) {
+
+    }
+
+    public void update(long id, String title) {
+
+    }
+
+    public void delete(long id) {
+
+    }
+
+    public void recover(long id) {
+
+    }
+
+}
